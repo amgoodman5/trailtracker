1f3638a5211f8a16c1ef6375be1ac6c6e979aec4
Aaron Goodman <Amgoodman5@Aarons-MacBook-Air.local>
2016-12-14 11:48:08 -0700

    styles and stuff

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../db/knex');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  knex('trail')
+  .then((data)=>{
+      res.render('index', { data:data });
+  });
+});
+
+router.get('/:id', function (req, res) {
+  knex('trail')
+    .where('id', req.params.id)
+    .first()
+    .then((data) => {
+      res.render('view', {
+        name: data.name,
+        distance: data.distance,
+        elevation_gain: data.elev_gain,
+        location: data.location
+      });
+    });
 });
 
 module.exports = router;
